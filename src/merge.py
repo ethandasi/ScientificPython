966da5382ec1ba258f5da57e27e4ecba3d14267e
SoumaGhazouani <souhaghazouani04@gmail.com>
2024-05-24 08:16:46 +0200

    update path

--- src/merge.py
+++ src/merge.py
@@ -175,12 +175,12 @@ if __name__ == "__main__":
     landmarks_2D_position = np.array([[1, 1], [1, -1], [-1, -1], [-1, 1]])
     points = [
         [0, 0],  # Starting point
-        [1, 0],  # Move forward 1 meter
-        [1, 0.5],  # Turn 90 degrees to the left and move forward 1 meter
-        [0, 2]   # Turn 90 degrees to the left and move forward 1 meter
+        [1, 0],  
+        [1, 0.5],  
+        [0, 1]   
     ]
     # Update the XML file with new positions
-    xml_file_path = "./XML/car_amers.xml"
+    xml_file_path = "../XML/car_amers.xml"
     update_flag_positions(xml_file_path, [points[0][0], points[0][1], 0.2], [points[-1][0], points[-1][1], 0.2])
-    car_controller = CarController("./XML/car_amers.xml", landmarks_2D_position)
+    car_controller = CarController(xml_file_path, landmarks_2D_position)
     car_controller.run(points)
